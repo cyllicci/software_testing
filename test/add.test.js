0dedc2583aa693acf9cf632c80ed4ff083168ede
Emma <emma.sjoholm@tuni.fi>
2023-12-06 16:11:43 +0200

    Separated tests and coverage even more

--- test/add.test.js
+++ test/add.test.js
@@ -8,4 +8,27 @@ describe("Add", () => {
  it("adds positive number to positive number", () =>{
      expect(add(2,6)).to.equal(8)
  });
+
+ it("adds zero to positive number", () => {
+    expect(add(2,0)).to.equal(2)
+    expect(add(0,2)).to.equal(2)
+ });
+
+ it("adds negative number to positive number", () => {
+    expect(add(-2,6)).to.equal(4)
+    expect(add(2,-6)).to.equal(-4)
+ });
+
+ it("adds zero and zero together", () => {
+    expect(add(0,0)).to.equal(0)
+ });
+
+ it("adds floating numbers correctly", () => {
+    expect(add(2.5, 3.5)).to.equal(6.0)
+ });
+
+ it("takes string as input", () => {
+    expect(add("2",3)).to.equal(5)
+ });
+
 })
