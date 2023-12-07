18be6df933ae556eb03cc4fecfdd471d9ca64020
Emma <emma.sjoholm@tuni.fi>
2023-12-07 03:45:28 +0200

    Delete unnecessary comments from filter test suite file

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,28 +1,3 @@
-/**
- * Iterates over elements of `array`, returning an array of all elements
- * `predicate` returns truthy for. The predicate is invoked with three
- * arguments: (value, index, array).
- *
- * **Note:** Unlike `remove`, this method returns a new array.
- *
- * @since 5.0.0
- * @category Array
- * @param {Array} array The array to iterate over.
- * @param {Function} predicate The function invoked per iteration.
- * @returns {Array} Returns the new filtered array.
- * @see pull, pullAll, pullAllBy, pullAllWith, pullAt, remove, reject
- * @example
- *
- * const users = [
- *   { 'user': 'barney', 'active': true },
- *   { 'user': 'fred',   'active': false }
- * ]
- *
- * filter(users, ({ active }) => active)
- * // => objects for ['barney']
- */
-//function filter(array, predicate)
-
 import chai from "chai"
 import filter from "../src/filter.js"
 
